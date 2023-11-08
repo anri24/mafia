7df7a727a6a7ea67ab5ee675d9ce790664c8ad73
anri <anrivarshanidze2407@gmail.com>
2023-11-08 17:33:37 +0400

    refactor code

--- app/Services/RoleService.php
+++ app/Services/RoleService.php
@@ -9,33 +9,29 @@ use Illuminate\Http\Request;
 
 class RoleService
 {
-    public function storeRoleStatistic(Request $request,Table $table)
+    public function storeRoleStatistic(Request $request, Table $table)
     {
-//        dd($request);
         $roles = Role::all();
+        foreach ($roles as $role) {
+            $role->name = str_replace(' ', '_', $role->name);
 
-
-        foreach ($roles as $role){
-
-            $role->name = str_replace(' ','_',$role->name);
-
-            if ($request->{$role->name.'_count'} == null){
-                $request->{$role->name.'_count'} = 0;
+            if ($request->{$role->name . '_count'} == null) {
+                $request->{$role->name . '_count'} = 0;
             }
 
-            if (isset($table->roleStatistic) && $table->roleStatistic()->where('role_id',$role->id)->exists()){
-                $table->roleStatistic()->where('role_id',$role->id)->first()->update([
-                    'count' => $request->{$role->name.'_count'}
+            if (isset($table->roleStatistic) && $table->roleStatistic()->where('role_id', $role->id)->exists()) {
+                $table->roleStatistic()->where('role_id', $role->id)->first()->update([
+                    'count' => $request->{$role->name . '_count'}
                 ]);
             } else {
-                    RoleStatistic::query()->create([
-                        'table_id' => $table->id,
-                        'role_id' => $role->id,
-                        'count' => $request->{$role->name.'_count'}
-                    ]);
+                RoleStatistic::query()->create([
+                    'table_id' => $table->id,
+                    'role_id' => $role->id,
+                    'count' => $request->{$role->name . '_count'}
+                ]);
             }
-
         }
-        return redirect()->route('admin.table.players',$table->id);
+
+        return redirect()->route('admin.table.players', $table->id);
     }
 }
