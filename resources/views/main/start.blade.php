d07570af28d54eb01d708a3fa7312dd30534a8e7
anri <anrivarshanidze2407@gmail.com>
2023-11-08 17:43:08 +0400

    changes

--- resources/views/main/start.blade.php
+++ resources/views/main/start.blade.php
@@ -13,20 +13,29 @@
     @endif
 </center>
     @else
+        @if($player->status == 0)
+            <div style="color: red">
+            <center>თქვენ მოკვდით</center><br>
+            </div>
+        @endif
         @foreach($player->table->players as $tablePlayer)
             <div style="border: 2px solid black" class="container px-4 text-center">
                 <div class="row gx-5">
                     <div class="col">
-                        <div class="p-3">{{ $tablePlayer->name }}</div>
+                        <div class="p-3"><b>{{ $tablePlayer->name }}</b></div>
                     </div>
+                    @if($player->status == 1)
                     <div class="col">
                         <div class="p-3">
+
                             <form method="post" action="{{ route('store.vote',[$player->id,$tablePlayer->id]) }}">
                                 @csrf
                                 <button type="submit" class="btn btn-danger">ხმის მიცემა</button>
                             </form>
+
                         </div>
                     </div>
+                    @endif
                 </div>
             </div>
         @endforeach
