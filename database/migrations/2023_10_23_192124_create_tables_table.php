29afdf7c2cac95af961ffc908c452d1d813e24f7
anri varshanidze <anrivarshanidze2407@gmail.com>
2023-11-27 11:44:13 +0400

    chore

--- database/migrations/2023_10_23_192124_create_tables_table.php
+++ database/migrations/2023_10_23_192124_create_tables_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->integer('fall');
-            $table->integer('users_quantity');
+            $table->integer('users_quantity')->nullable();
             $table->integer('status')->default(0);
             $table->timestamps();
         });
